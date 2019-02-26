d65c5e0f65156aff297342e113b621edac60987e
Keith Saniga <sanigak@outlook.com>
2019-02-26 13:40:58 -0500

    successful Hello World

--- LearningDjango/LearningDjango/urls.py
+++ LearningDjango/LearningDjango/urls.py
@@ -3,12 +3,18 @@ Definition of urls for LearningDjango.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+
+
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+
     # Examples:
     # url(r'^$', LearningDjango.views.home, name='home'),
     # url(r'^LearningDjango/', include('LearningDjango.LearningDjango.urls')),
